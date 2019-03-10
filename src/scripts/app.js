b7fbf612a9a69914926bc8b6371e1cd886124143
Moin <moin.g4@gmail.com>
2019-03-10 13:38:17 -0400

    updated

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,16 @@
-const hello = 'World';
-console.log(hello);
+$(document).ready(function () {
+    setTimeout(function () {
+        $('body').addClass('loaded');
+    }, 1000);    
+
+    $('a').smoothScroll({
+        speed: 600
+    });
+
+    $(window).resize(function () {
+        if($(window).width() < 500) {
+            $('.hvr-grow').removeClass('hvr-grow');
+        }
+    });
+
+});
